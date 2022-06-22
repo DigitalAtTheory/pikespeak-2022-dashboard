0832549ba301776e39bfb402a932f74c055eb1bc
Josh Suson <digital@ilovetheory.com>
2022-06-22 14:09:24 -0400

    Working on changing dashboard over

--- app/routes/entries.js
+++ app/routes/entries.js
@@ -4,18 +4,19 @@ import { db } from "../../firebase/db";
 
 export const action = async ({ request }) => {
   const body = await request.json();
-  const colRef = collection(db, "ncmbash-2022");
-  const entry = {
-    location:
-      body.AreYouAttendingTheNCMBashInPerson === "I'm at the event!"
-        ? "onsite"
-        : "virtual",
-    basicInformation: body.BasicInformation,
-  };
+  // const colRef = collection(db, "ncmbash-2022");
+  // const entry = {
+  //   location:
+  //     body.AreYouAttendingTheNCMBashInPerson === "I'm at the event!"
+  //       ? "onsite"
+  //       : "virtual",
+  //   basicInformation: body.BasicInformation,
+  // };
 
-  addDoc(colRef, entry);
-
-  console.log(entry);
+  // addDoc(colRef, entry);
+  console.log("Hi server");
+  console.log(body);
+  // console.log(entry);
 
   return json({ success: true }, 200);
 };
