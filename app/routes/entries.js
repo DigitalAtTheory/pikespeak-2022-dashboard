a1834a6de32fba8501fb1321e78c68fd6a5c62c8
Josh Suson <digital@ilovetheory.com>
2022-06-22 16:37:30 -0400

    Adding other surveys

--- app/routes/entries.js
+++ app/routes/entries.js
@@ -5,20 +5,44 @@ import { db } from "../../firebase/db";
 export const action = async ({ request }) => {
   const body = await request.json();
 
-  if (body.Form?.Id === "110") {
-    const colRef = collection(db, "hill-grind-2022");
-    const entry = {
-      basicInformation: body.BasicInformation,
-      location:
-        body.AreYouAttendingThePikesPeakInPerson === "I'm at the event!"
-          ? "onsite"
-          : "virtual",
-    };
+  switch (body.Form?.Id) {
+    case "110":
+      const hillCol = collection(db, "hill-grind-2022");
+      const hillEntry = {
+        basicInformation: body.BasicInformation,
+        location:
+          body.AreYouAttendingThePikesPeakInPerson === "I'm at the event!"
+            ? "onsite"
+            : "virtual",
+      };
 
-    console.log("logging entry");
-    await addDoc(colRef, entry);
-    console.log("entry logged");
-    console.log(entry);
+      console.log("logging entry");
+      await addDoc(hillCol, hillEntry);
+      console.log("entry logged");
+      console.log(hillEntry);
+    case "109":
+      const retailCol = collection(db, "pikes-retail-2022");
+      const onsiteEntry = {
+        basicInformation: body.BasicInformation,
+        location: "onsite",
+      };
+
+      console.log("logging entry");
+      await addDoc(retailCol, onsiteEntry);
+      console.log("entry logged");
+      console.log(onsiteEntry);
+    case "108":
+      const virtualEntry = {
+        basicInformation: body.BasicInformation,
+        location: "virtual",
+      };
+
+      console.log("logging entry");
+      await addDoc(retailCol, virtualEntry);
+      console.log("entry logged");
+      console.log(virtualEntry);
+    default:
+      console.log("This from is not being tracked on this dashboard ");
   }
 
   return json({ success: true }, 200);
