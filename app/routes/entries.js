6bff6b91133a55e4895faef2a2208bc944eb7e6b
Josh Suson <digital@ilovetheory.com>
2022-06-22 14:58:47 -0400

    Dashboard work

--- app/routes/entries.js
+++ app/routes/entries.js
@@ -4,19 +4,23 @@ import { db } from "../../firebase/db";
 
 export const action = async ({ request }) => {
   const body = await request.json();
-  // const colRef = collection(db, "ncmbash-2022");
-  // const entry = {
-  //   location:
-  //     body.AreYouAttendingTheNCMBashInPerson === "I'm at the event!"
-  //       ? "onsite"
-  //       : "virtual",
-  //   basicInformation: body.BasicInformation,
-  // };
 
-  // addDoc(colRef, entry);
+  if (body.Form.Id === "110") {
+    const colRef = collection(db, "hill-grind-2022");
+    const entry = {
+      basicInformation: body.BasicInformation,
+      location:
+        body.AreYouAttendingThePikesPeakInPerson === "I'm at the event!"
+          ? "onsite"
+          : "virtual",
+    };
+
+    addDoc(colRef, entry);
+    console.log(entry);
+  }
+
   console.log("Hi server");
   console.log(body);
-  // console.log(entry);
 
   return json({ success: true }, 200);
 };
